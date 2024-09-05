fe4d84d21af8581ba54fc5c0ceb183a73e80fb2f
Orlov <orlov-druxa@mail.ru>
2024-09-06 02:47:40 +0300

    Criteria

--- app/Application.py
+++ app/Application.py
@@ -5,7 +5,7 @@ from axe_selenium_python import Axe
 driver = webdriver.Chrome()
 
 # Открытие веб-сайта
-driver.get('https://doka.guide/a11y/?view=themes')
+driver.get('https://nuoflix.de')
 
 # Инициализация Axe
 axe = Axe(driver)
@@ -16,7 +16,6 @@ results = axe.run()
 
 # Закрытие драйвера
 driver.quit()
-
 
 # Функция для расчета балла
 def calculate_accessibility_score(results):
@@ -50,9 +49,30 @@ def calculate_accessibility_score(results):
     final_score = max(min(score, 100), 0)
     return final_score
 
+# Функция для вывода ошибок по важным критериям
+def print_important_violations(results):
+    important_criteria = [
+        'keyboard',  # Использование клавиатуры (tab)
+        'zoom',  # Увеличение до 200%
+        'document-title',  # Заголовки сайта
+        'image-alt',  # Текст в картинках
+        'color-contrast',  # Контрастность текста
+        'aria-required-children',  # Динамические элементы
+        'label'  # Описание форм
+    ]
+
+    print("\nОшибки по важным критериям:")
+    for violation in results['violations']:
+        if violation['id'] in important_criteria:
+            print(f"\nКритерий: {violation['id']}")
+            print(f"Описание: {violation['description']}")
+            print(f"Количество элементов с ошибками: {len(violation['nodes'])}")
 
 # Расчет итогового балла
 final_score = calculate_accessibility_score(results)
 
 # Вывод результата
 print(f"Итоговый балл доступности: {final_score:.2f}/100")
+
+# Вывод ошибок по важным критериям
+print_important_violations(results)
